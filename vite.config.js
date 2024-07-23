e4ab76c5b1dc7fe720dbb62185f45f7d409a0b14
Rahman Hafidzin <97663781+Rhafidzin@users.noreply.github.com>
2024-07-23 13:59:24 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills(),],
-  base: "/order/product"
+  base: "/order/product",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
